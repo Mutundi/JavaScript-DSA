76ad9dcdab0162059dd18b4fdc211dbb24163d60
Mutundi Levi <levimutundi@gmail.com>
2024-04-09 12:21:51 +0300

    A program to change the style of a text at runtime.

---
+++ 3-Program_change_style/index.js
@@ -0,0 +1,11 @@
+//A function that change the style of a text at runtime.
+
+const changeStyle = () => {
+    document.getElementById("change").style.color = "red";
+    document.getElementById("change").style.backgroundColor = "gray";
+    document.getElementById("change").style.width = "80px";
+    document.getElementById("change").style.height = "40px";
+    document.getElementById("change").style.borderRadius = "10%";
+    document.getElementById("change").style.alignContent = "center";
+    document.getElementById("change").style.textAlign = "center";
+};
