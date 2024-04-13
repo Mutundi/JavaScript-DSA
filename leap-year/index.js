a44df16bd3ad04f9feec6de313fcb722d619500c
Mutundi Levi <levimutundi@gmail.com>
2024-04-13 12:24:47 +0300

    A script that calculates whether a given year is a leap year or not.

---
+++ leap-year/index.js
@@ -0,0 +1,25 @@
+
+const isLeapYear = (year) => {
+    //Step 1: Check if year is evenly divisible by 4, if yes go to step 2. Otherwise, it is not a leap year.
+    if (year % 4 === 0) {
+        //Step 2: Check if year is evenly divisible by 100, if it is go to step 3. Otherwise, it is a leap year.
+        if (year % 100 === 0) {
+            //Step 3: Check if year is evenly divisible by 400, if yes it is a leap. Otherwise, it is not.
+            if (year % 400 === 0) {
+                return true; //It is a leap year.
+            }else{
+                return false; //It is not a leap year.
+            }
+        }else{
+            return true; //It is a leap year.
+        }
+    }else{
+        return false; //It is not a leap year.
+    }
+};
+
+
+console.log(isLeapYear(2100));
+console.log(isLeapYear(2000));
+console.log(isLeapYear(1900));
+console.log(isLeapYear(2008));
