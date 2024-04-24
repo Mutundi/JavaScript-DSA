449744990b82c2fa5a34e1622c357f681d23ad2a
Mutundi Levi <levimutundi@gmail.com>
2024-04-24 19:09:22 +0300

    A script that checks whether a given integer is within 20 of 100 or 400.

--- within_range/index.js
+++ within_range/index.js
@@ -4,13 +4,15 @@ const isWithinRange = (num) => {
     return ((Math.abs(100 - num) <= 20) || (Math.abs(400 - num) <= 20));
 };
 
+
+//Test the function.
 const num1 = 90;
 const num2 = 115;
 const num3 = 380;
 const num4 = 250;
 const num5 = 418;
 
-//Test the function.
+
 console.log(num1 + " is within 20 of 100 or 400: " + isWithinRange(num1));
 console.log(num2 + " is within 20 of 100 or 400: " + isWithinRange(num2));
 console.log(num3 + " is within 20 of 100 0r 400: " + isWithinRange(num3));
